586e8e57aa4697243ee37533721b7632c54112fd
Euris Villa <evc1893@gmail.com>
2025-10-23 12:22:17 -0500

    Production settings for PA. Python 3.10 downgrade for PA compatibility

--- mtapp/settings/production.py
+++ mtapp/settings/production.py
@@ -7,8 +7,39 @@ DEBUG = False
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='www.milanotravel.com.ec')]
+
 
 try:
     from .local import *
 except ImportError:
     pass
+
+# 5. Security Settings
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# HTTPS in production
+SECURE_SSL_REDIRECT = True  # if behind proxy, see below
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # Enforce HTTPS for 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Sensitive keys from environment variables
+SECRET_KEY = config('SECRET_KEY')
+OPEN_EXCHANGE_RATES_API_KEY = config('OPEN_EXCHANGE_RATES_API_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+GOOGLE_TRANSLATE_KEY = config('GOOGLE_TRANSLATE_KEY')
+
+# Email settings for production
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='mail.milanotravel.com.ec')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'info@milanotravel.com.ec'
