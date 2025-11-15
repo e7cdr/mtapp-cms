f8dbb80e0fc5c901ebc360d05b18cc6b1b7b3f92
Euris Villa <evc1893@gmail.com>
2025-11-14 20:50:16 -0500

    Autoplay Basic Swiper

--- mtapp/static/js/swipers/basic_swiper.js
+++ mtapp/static/js/swipers/basic_swiper.js
@@ -27,10 +27,10 @@ document.addEventListener('DOMContentLoaded', function(){
                     spaceBetween: 20,
                 }
             },
-            // autoplay: {
-            //     delay: 4000,
-            //     pauseOnMouseEnter: true,
-            // },
+            autoplay: {
+                delay: 4000,
+                pauseOnMouseEnter: true,
+            },
 
             speed: 2000,
 
